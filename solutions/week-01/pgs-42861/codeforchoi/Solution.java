fd65da03fc48d83cb1367539dac8445c2241214a
codeforchoi <codeforchoi@gmail.com>
2026-09-18 13:46:23 +0900

    solve: pgs-42861 네트워크

---
+++ solutions/week-01/pgs-42861/codeforchoi/Solution.java
@@ -0,0 +1,76 @@
+class Solution {
+    public int solution(int n, int[][] computers) {
+        boolean[] visited = new boolean[n];
+        int answer = 0;
+
+        for(int i = 0; i < n; i++){
+            if(!visited[i]){
+                dfs(i, computers, visited);
+                answer++;
+            }
+        }
+        return answer;
+    }
+    
+    private static void dfs(int cur, int[][] computers, boolean[] vistied) {
+        vistied[cur] = true;
+
+        for(int i = 0; i < computers.length; i++) {
+            if(computers[cur][i] == 1 && !vistied[i]) {
+                dfs(i, computers, vistied);
+            }
+        }
+    }
+}
+
+/*
+import java.util.*;
+
+class Solution {
+    private static int[] parent;
+
+	public static int solution(int n, int[][] computers) {
+		List<Integer>[] graph = new ArrayList[n];
+		for(int i = 0; i < n; i++) {
+			graph[i] = new ArrayList<>();
+		}
+		
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				if(i != j && computers[i][j] == 1) {
+					graph[i].add(j);	
+				}
+			}
+		}
+		
+		parent = new int[n];
+		for(int i = 0; i < n; i++) {
+			parent[i] = i;
+		}
+		
+		int count = 0;
+		for(int i = 0; i < n; i++) {
+			for(int v : graph[i]) {
+				if(count == n - 1) break;
+				if(find(i) != find(v)) {
+					union(i, v);
+					count++;
+				}
+			}
+		}
+		
+        return n - count;
+    }
+	
+	private static int find(int n) {
+		if(parent[n] == n) return n;
+		return parent[n] = find(parent[n]);
+	}
+	
+	private static void union(int u, int v ) {
+		u = find(u);
+		v = find(v);
+		if(u != v) parent[u] = v;
+	}
+}
+*/
